59a00c5db08798fd036d73fd7320e9fddcddd448
Ataby <matabay200069@gmail.com>
2024-10-14 22:47:04 +0300

    card slider

--- app/components/CardSlider.jsx
+++ app/components/CardSlider.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import styles from "../styles/globals.scss";
+
 
 const CardSlider = ({ newsData }) => {
   return (
-    <div className="whole-page">
+    <div className="whole-page" >
       {newsData.map((article, index) => (
         <div className="card" key={index}>
-          <img src={article.image} alt={article.title} />
-          <h2 className="card-title">{article.title}</h2>
-          <p className="card-description">{article.description}</p>
+          <div className="card-image">
+            <img src={article.image} alt={article.title} />
+          </div>
+          <div className="card-title">
+            <h2>{article.title}</h2>
+          </div>
+          {/* <div className="card-description">
+            <p>{article.description}</p>
+          </div> */}
         </div>
       ))}
     </div>
