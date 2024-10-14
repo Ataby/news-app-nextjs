e40c8159f36a2f214c6288be37db9b523296da8d
Ataby <matabay200069@gmail.com>
2024-10-14 22:46:50 +0300

    editted

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import styles from "./page.module.css";
 import Index from "./pages/index";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div >
       <Index/>
     </div>
   );
